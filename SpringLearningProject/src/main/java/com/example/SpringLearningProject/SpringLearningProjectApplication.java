73262352179d00896e320ec53bab5c3167f4ac93
ZulfiqarAli111 <zulfiqarali.bscssef20@iba-suk.edu.pk>
2026-07-27 10:56:13 +0500

    learning spring boot advance

---
+++ SpringLearningProject/src/main/java/com/example/SpringLearningProject/SpringLearningProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringLearningProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearningProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearningProjectApplication.class, args);
+	}
+
+}
---
+++ SpringLearningProject/src/test/java/com/example/SpringLearningProject/SpringLearningProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringLearningProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearningProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
